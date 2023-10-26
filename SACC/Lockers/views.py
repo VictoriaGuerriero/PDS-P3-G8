c9b24d0ade52151aef29c271d9ebe7edd6b97941
Victoria Guerriero <vaguerriero@miuandes.cl>
2023-10-26 16:28:58 -0300

    feat: created some Lockers views

--- SACC/Lockers/views.py
+++ SACC/Lockers/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework import viewsets
+from .serializers import StationSerializer, LockerSerializer
+from .models import Station, Locker
+from django.http import JsonResponse
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+    @action(
+        detail=True, 
+        methods=['delete']
+    )
+    def delete_station(self, request, pk):
+        station = Station.objects.get(id=pk)
+        station.delete()
+        return JsonResponse({'message': 'Station deleted'}, status=200)
+    
+class LockerViewSet(viewsets.ModelViewSet):
+    queryset = Locker.objects.all()
+    serializer_class = LockerSerializer
+
+    @action(
+        detail=True, 
+        methods=['delete']
+    )
+    def delete_locker(self, request, pk):
+        locker = Locker.objects.get(id=pk)
+        locker.delete()
+        return JsonResponse({'message': 'Locker deleted'}, status=200)
+
+    
 
 # Create your views here.
